c0ed4cb392c1c61f62117749bdb6700747799975
apdavid <abeldavidmd@gmail.com>
2025-10-28 22:49:13 -0400

    updated slurm executable

--- run_prioritize.sh
+++ run_prioritize.sh
@@ -3,21 +3,28 @@
 #SBATCH --output=../logs/prioritize_plot_%j.out
 #SBATCH --error=../logs/prioritize_plot_%j.err
 #SBATCH --time=01:00:00
-#SBATCH --mem=8G
+#SBATCH --mem=64G
 #SBATCH --cpus-per-task=2
 
-# Define arguments
-NAME="petipre_sgn_2022"
+CONDA_ENV=nf-spliz-env
+NAME=petipre_sgn_2022
 OUTPATH=${NAME}
 GROUP1=cell_type
 GROUP2=developmental_age
 CUTOFF=0.25
+RESULTS_DIR=/data/l2_jan_lab/abel/SpliZ/results/${NAME}
+PLOTTERFILE=${RESULTS_DIR}/SpliZ_sites/first_evec_${NAME}_pvals_${GROUP2}-${GROUP1}_100_S_0.1_z_0.0_b_5.tsv
+SVD=${RESULTS_DIR}/SpliZ_values/${NAME}_sym_SVD_normdonor_S_0.1_z_0.0_b_5.pq
 
-# Define file paths: first eigenvector and SVD file
-PLOTTERFILE=/data/l2_jan_lab/abel/SpliZ/results/${NAME}/SpliZ_sites/first_evec_${NAME}_pvals_${GROUP2}-${GROUP1}_100_S_0.1_z_0.0_b_5.tsv
-SVD=/data/l2_jan_lab/abel/SpliZ/results/${NAME}/SpliZ_values/${NAME}_sym_SVD_normdonor_S_0.1_z_0.0_b_5.pq
+echo "----------------------------------------------"
+echo "Data Name             : $NAME"
+echo "Grouping Level 1      : $GROUP1"
+echo "Grouping Level 2      : $GROUP2"
+echo "Cutoff                : $CUTOFF"
+echo "----------------------------------------------"
 
-# Run the script
+conda activate "$CONDA_ENV"
+
 python prioritize_plotting.py \
     --outpath "$OUTPATH" \
     --plotterFile "$PLOTTERFILE" \
